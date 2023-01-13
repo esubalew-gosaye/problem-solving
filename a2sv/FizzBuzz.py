cc849fb597350fd09adc695f43c80b3b5f625e78
Esubalew <91283106+esubalew-gosaye@users.noreply.github.com>
2023-01-13 15:53:24 +0300

    Update FizzBuzz.py

--- a2sv/FizzBuzz.py
+++ a2sv/FizzBuzz.py
@@ -1,1 +1,10 @@
-fiz
+class Solution:
+   
+    def fizzBuzz(self, n: int) -> List[str]:
+        l = []
+        for i in range(n+1):
+            if i%3==0 and i%5==0:l+=['FizzBuzz']
+            elif i%3==0: l+=['Fizz']
+            elif i%2==0: l+=['Buzz']
+            else: l+=[i]
+        return l
