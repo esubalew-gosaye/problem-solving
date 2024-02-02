2996e8b7650f75b2b017dba1f31dbc331db78eab
Esubalew <91283106+esubalew-gosaye@users.noreply.github.com>
2024-02-02 21:34:24 +0300

    Create python-arithmetic-operators.py

---
+++ a2sv/python-arithmetic-operators.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    number_1= int(input())
+    number_2= int(input())
+    
+    print(number_1 + number_2)
+    print(number_1 - number_2)
+    print(number_1 * number_2)
