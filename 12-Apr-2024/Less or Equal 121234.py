39582b9d63484054abd8e172b2daaa79ac187fd2
Esubalew <91283106+esubalew-gosaye@users.noreply.github.com>
2024-04-12 09:16:44 +0300

    Created 'Less or Equal' by A2SV Hub

---
+++ 12-Apr-2024/Less or Equal 121234.py
@@ -0,0 +1,23 @@
+# Problem: Less or Equal - https://codeforces.com/problemset/problem/977/C
+
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+arr.sort()
+
+ans = arr[k - 1]
+
+if k == 0:
+    ans = arr[0] - 1
+    
+
+if n > k:
+    if arr[k] > ans and ans >= 1 and ans <= 1000 * 1000 * 1000:
+        print(ans)
+    else:
+        print(-1)
+else:
+    if ans >= 1 and ans <= 1000 * 1000 * 1000:
+        print(ans)
+    else:
+        print(-1)
