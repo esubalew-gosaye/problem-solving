0ef0d86bda84f857f5c321992e092099e7a3b982
Esubalew <91283106+esubalew-gosaye@users.noreply.github.com>
2024-04-12 10:01:45 +0300

    Created 'Ehab Is an Odd Person' by A2SV Hub

---
+++ 12-Apr-2024/Ehab Is an Odd Person 121256.py
@@ -0,0 +1,9 @@
+# Problem: Ehab Is an Odd Person - https://codeforces.com/problemset/problem/1174/B
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+if all(arr[i - 1] % 2 == arr[i] % 2 for i in range(1, n)):
+    print(*arr)
+else:
+    print(*sorted(arr))
