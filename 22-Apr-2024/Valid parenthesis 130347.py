be379f1aed37569bb354a2b14612f81ca01eda46
Esubalew <91283106+esubalew-gosaye@users.noreply.github.com>
2024-04-22 21:13:37 +0300

    Created 'Valid parenthesis' by A2SV Hub

---
+++ 22-Apr-2024/Valid parenthesis 130347.py
@@ -0,0 +1,19 @@
+# Problem: Valid parenthesis - https://leetcode.com/problems/valid-parentheses/
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        brackets = {"{":"}", "(":")", "[":"]"}
+
+        for char in s:
+            if char in brackets.keys():
+                stack.append(char)
+            else:
+                if stack == []:
+                    return False
+                    
+                closing_bracket = stack.pop()
+                if char != brackets[closing_bracket]:
+                    return False                    
+                
+        return stack == []
