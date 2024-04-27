877f0f05c670a621632cbd3af5a1702d8f8e5027
Esubalew <91283106+esubalew-gosaye@users.noreply.github.com>
2024-04-27 21:27:43 +0300

    Created 'A - FIA Heist' by A2SV Hub

---
+++ A2SV Remote G5 Contest 12 27-Apr-2024/A - FIA Heist 135068.py
@@ -0,0 +1,21 @@
+# Problem: A - FIA Heist - https://codeforces.com/gym/520098/problem/A
+
+s = input()
+
+stack = []
+
+is_back = False
+
+for i in s:
+
+    if i == '<':
+        is_back = True
+        if stack:
+            stack.pop()
+    elif is_back and i == "-":
+        is_back = False
+        continue
+    else:
+        stack.append(i)
+
+print("".join(stack))
