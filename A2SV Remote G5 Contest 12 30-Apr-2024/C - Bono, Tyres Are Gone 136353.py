046a5c13b0d54665ec03659c2980faa22f6b0981
Esubalew <91283106+esubalew-gosaye@users.noreply.github.com>
2024-04-30 10:01:50 +0300

    Created 'C - Bono, Tyres Are Gone' by A2SV Hub

---
+++ A2SV Remote G5 Contest 12 30-Apr-2024/C - Bono, Tyres Are Gone 136353.py
@@ -0,0 +1,24 @@
+# Problem: C - Bono, Tyres Are Gone - https://codeforces.com/gym/520098/problem/C
+
+n = int(input())
+
+stack = []
+reorder = 0
+cnt_rmv = 1
+for i in range(2 * n):
+    cmd = input().split()
+
+    if cmd[0] == "add":
+        stack.append(int(cmd[1]))
+
+    elif cmd[0] == "remove" and stack:
+        
+        if stack[-1] != cnt_rmv:
+            reorder += 1
+            stack.clear()
+        else:
+            stack.pop()
+        cnt_rmv += 1
+    else:
+        cnt_rmv += 1
+print(reorder)
