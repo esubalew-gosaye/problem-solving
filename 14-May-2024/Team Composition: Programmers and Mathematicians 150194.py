d89de684d4cb89a0dfa3bb9be2a7b465ae410955
Esubalew <91283106+esubalew-gosaye@users.noreply.github.com>
2024-05-14 21:28:14 +0300

    Created 'Team Composition: Programmers and Mathematicians' by A2SV Hub

---
+++ 14-May-2024/Team Composition: Programmers and Mathematicians 150194.py
@@ -0,0 +1,18 @@
+# Problem: Team Composition: Programmers and Mathematicians - https://codeforces.com/problemset/problem/1611/B
+
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+
+    low = 0
+    high = min(a, b)
+    ans = 0
+    while low <= high:
+
+        mid = (low + high) // 2
+
+        if a >= mid and b >= mid and a + b >= mid * 4:
+            ans = max(ans, mid)
+            low = mid + 1
+        else:
+            high = mid - 1
+    print(ans)
