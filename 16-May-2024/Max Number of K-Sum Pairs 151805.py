b7dc69140b4c119fba0d67daaa769588989f16b5
Esubalew <91283106+esubalew-gosaye@users.noreply.github.com>
2024-05-16 08:39:31 +0300

    Created 'Max Number of K-Sum Pairs' by A2SV Hub

---
+++ 16-May-2024/Max Number of K-Sum Pairs 151805.py
@@ -0,0 +1,21 @@
+# Problem: Max Number of K-Sum Pairs - https://leetcode.com/problems/max-number-of-k-sum-pairs/
+
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+
+        left = 0 
+        right = len(nums) - 1
+        count = 0
+        while left < right:
+
+            if nums[left] + nums[right] == k:
+                print(left, right)
+                count += 1
+                left += 1
+                right -= 1
+            elif nums[left] + nums[right] > k:
+                right -= 1
+            else:
+                left += 1
+        return count 
