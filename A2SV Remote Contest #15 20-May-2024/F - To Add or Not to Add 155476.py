fccf8aa03eac8eece8c61ac87ae86e1f1ee26fbe
Esubalew <91283106+esubalew-gosaye@users.noreply.github.com>
2024-05-20 22:25:41 +0300

    Created 'F - To Add or Not to Add' by A2SV Hub

---
+++ A2SV Remote Contest #15 20-May-2024/F - To Add or Not to Add 155476.py
@@ -0,0 +1,22 @@
+# Problem: F - To Add or Not to Add - https://codeforces.com/gym/524150/problem/F
+
+a, k = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+
+left = 0
+summation = 0
+freq = 0
+num = nums[0]
+
+for right in range(a):
+    summation += nums[right]
+
+    while k < nums[right] * (right - left + 1) - summation:
+        summation -= nums[left]
+        left += 1
+    
+    if right - left + 1 > freq:
+        freq = right - left + 1
+        num = nums[right]
+    
+print(freq, num)
