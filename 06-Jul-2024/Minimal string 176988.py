b51fef12a5947beb5d882849ea2e5e60041f0f54
Esubalew <91283106+esubalew-gosaye@users.noreply.github.com>
2024-07-06 23:29:29 +0300

    Created 'Minimal string' by A2SV Hub

---
+++ 06-Jul-2024/Minimal string 176988.py
@@ -0,0 +1,24 @@
+# Problem: Minimal string - https://codeforces.com/contest/797/problem/C
+
+from collections import Counter
+
+def minimal_string_game(s):
+    t = []
+    u = []
+    counter = Counter(s)
+    
+    for ch in s:
+        t.append(ch)
+        counter[ch] -= 1
+        
+        while t and (not any(counter[ch] for ch in counter if ch < t[-1])):
+            u.append(t.pop())
+    
+    return ''.join(u)
+
+
+s = input().strip()
+
+result = minimal_string_game(s)
+
+print(result)
