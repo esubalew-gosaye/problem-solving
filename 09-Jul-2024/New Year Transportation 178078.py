3b47c51aec714c3cc90cd48a1f23536efa0811a9
Esubalew <91283106+esubalew-gosaye@users.noreply.github.com>
2024-07-09 10:52:17 +0300

    Created 'New Year Transportation' by A2SV Hub

---
+++ 09-Jul-2024/New Year Transportation 178078.py
@@ -0,0 +1,16 @@
+# Problem: New Year Transportation - https://codeforces.com/problemset/problem/500/A
+
+n, t = list(map(int, input().split()))
+transportation = list(map(int, input().split()))
+
+flag = False 
+cell = 1
+
+while t >= cell:
+    if t == cell:
+        flag = True 
+        break
+
+    cell = cell + transportation[cell - 1]
+print("YES" if flag else "NO")
+    
