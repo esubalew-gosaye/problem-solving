a734ed7b28f5c3cbd165b62e98ffc874f43a05d1
Esubalew <91283106+esubalew-gosaye@users.noreply.github.com>
2024-07-16 16:56:51 +0300

    Created 'B - Guess the Maximum' by A2SV Hub

---
+++ A2SV Remote Contest #23 16-Jul-2024/B - Guess the Maximum 180829.py
@@ -0,0 +1,12 @@
+# Problem: B - Guess the Maximum - https://codeforces.com/gym/535309/problem/B
+
+for _ in range(int(input())):
+    n = int(input())
+    v = list(map(int, input().split()))
+
+    count = float('inf')
+    for i in range(n - 1):
+        ans = max(v[i], v[i + 1])
+        count = min(count, ans)
+    
+    print(count - 1)
