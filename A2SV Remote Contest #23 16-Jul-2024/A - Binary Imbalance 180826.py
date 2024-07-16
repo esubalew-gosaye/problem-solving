415ab962eb5221cf369cb6ec2c8bf0fa036e5c6d
Esubalew <91283106+esubalew-gosaye@users.noreply.github.com>
2024-07-16 16:40:42 +0300

    Created 'A - Binary Imbalance' by A2SV Hub

---
+++ A2SV Remote Contest #23 16-Jul-2024/A - Binary Imbalance 180826.py
@@ -0,0 +1,9 @@
+# Problem: A - Binary Imbalance - https://codeforces.com/gym/535309/problem/A
+
+for _ in range(int(input())):
+    input()
+    s = input()
+    if "0" in s:
+        print("YES")
+    else:
+        print("NO")
