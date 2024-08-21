d02b14e993bf602f9026b0d6dc18b57aee842dad
Esubalew <91283106+esubalew-gosaye@users.noreply.github.com>
2024-08-21 22:48:17 +0300

    Created 'C - Hello World' by A2SV Hub

---
+++ A2SV Remote G5 Education Contest #28 21-Aug-2024/C - Hello World 198514.py
@@ -0,0 +1,13 @@
+# Problem: C - Hello World - https://codeforces.com/gym/543262/problem/C
+
+test = int(input())
+for _ in range(test):
+    size = int(input())
+    array = sorted(list(map(int, input().split())))
+    currSum = 1 
+    valid = True if array[0] == 1 else False
+    for num in array[1:]:
+        if num > currSum:
+            valid = False
+        currSum += num
+    print('YES' if valid else 'NO')
