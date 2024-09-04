fd0dd1c2c53e7678161abf885c54edfea065d824
Esubalew <91283106+esubalew-gosaye@users.noreply.github.com>
2024-09-04 21:14:41 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 04-Sep-2024/Cirno's Perfect Bitmasks Classroom 211082.py
@@ -0,0 +1,13 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+tests = int(input())
+
+for _ in range(tests):
+    x = int(input())
+    y = x & -x
+
+    while True:
+        if x ^ y > 0 and x & y > 0:
+            break
+        y += 1
+    print(y)
