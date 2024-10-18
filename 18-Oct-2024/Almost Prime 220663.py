138772a9746c7eb7f93e59ca8b0fe6afeb1bb89c
Esubalew <91283106+esubalew-gosaye@users.noreply.github.com>
2024-10-18 21:56:15 +0300

    Created 'Almost Prime' by A2SV Hub

---
+++ 18-Oct-2024/Almost Prime 220663.py
@@ -0,0 +1,18 @@
+# Problem: Almost Prime - https://codeforces.com/problemset/problem/26/A
+
+n = int(input())
+
+count = 0
+for num in range(n + 1):
+    ls = set()
+    d = 2
+    while d * d <= num:
+        while num % d == 0:
+            ls.add(d)
+            num = num // d 
+        d += 1
+    if num > 1:
+        ls.add(num)
+    if len(ls) == 2:
+        count += 1
+print(count)
