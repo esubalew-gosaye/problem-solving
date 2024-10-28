7bae99f61d9277a8a505b8f2f0c1e2978ccf1aba
Esubalew <91283106+esubalew-gosaye@users.noreply.github.com>
2024-10-28 21:40:24 +0300

    Created 'B - Discounts' by A2SV Hub

---
+++ A2SV Remote Contest #34 28-Oct-2024/B - Discounts 222404.py
@@ -0,0 +1,10 @@
+# Problem: B - Discounts - https://codeforces.com/gym/559568/problem/B
+
+_ = input()
+a = list(map(int, input().split()))
+c = input()
+m = list(map(int, input().split()))
+a.sort(reverse=True)
+t_sum = sum(a)
+for q in range(int(c)):
+    print(t_sum - a[m[q] - 1])
