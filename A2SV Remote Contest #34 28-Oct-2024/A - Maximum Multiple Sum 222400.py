b774589d6237f3c89dd9f56c9cb8f83464df602d
Esubalew <91283106+esubalew-gosaye@users.noreply.github.com>
2024-10-28 21:08:41 +0300

    Created 'A - Maximum Multiple Sum' by A2SV Hub

---
+++ A2SV Remote Contest #34 28-Oct-2024/A - Maximum Multiple Sum 222400.py
@@ -0,0 +1,10 @@
+# Problem: A - Maximum Multiple Sum - https://codeforces.com/gym/559568/problem/A
+
+t = int(input())
+for _ in range(t):
+    d = int(input())
+
+    if d == 3:
+        print(d)
+    else:
+        print(2)
