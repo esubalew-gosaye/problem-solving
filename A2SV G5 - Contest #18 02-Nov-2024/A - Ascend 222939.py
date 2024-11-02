bae9e0a3c4dff1892d843b9294781d7960cfc94a
Esubalew <91283106+esubalew-gosaye@users.noreply.github.com>
2024-11-02 22:50:16 +0300

    Created 'A - Ascend' by A2SV Hub

---
+++ A2SV G5 - Contest #18 02-Nov-2024/A - Ascend 222939.py
@@ -0,0 +1,23 @@
+# Problem: A - Ascend - https://codeforces.com/gym/526229/problem/A
+
+def subarray_length(n, array):
+    if n == 1:
+        return 1
+
+    max_length = 1
+    current_length = 1
+
+    for i in range(1, n):
+        if array[i] > array[i - 1]:
+            current_length += 1
+        else:
+            max_length = max(max_length, current_length)
+            current_length = 1
+
+    max_length = max(max_length, current_length)
+
+    return max_length
+
+n = int(input())
+array = list(map(int, input().split()))
+print(subarray_length(n, array))
