fe69e9ff4deaf8878c823a8aeb556b40841bf13c
Esubalew <91283106+esubalew-gosaye@users.noreply.github.com>
2024-11-25 22:29:40 +0300

    Created 'C - Pairs' by A2SV Hub

---
+++ A2SV Remote G5 Contest II 25-Nov-2024/C - Pairs 224332.py
@@ -0,0 +1,12 @@
+# Problem: C - Pairs - https://codeforces.com/gym/504384/problem/C
+
+from collections import Counter
+n = int(input())
+matrix = [list(map(int, input().split())) for _ in range(3)]
+
+first = Counter(matrix[0])
+
+cnt = 0
+for i in range(n):
+    cnt += first.get(matrix[1][matrix[2][i] - 1], 0)
+print(cnt)
